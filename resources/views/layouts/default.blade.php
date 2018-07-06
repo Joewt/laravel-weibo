67fc22e58c88cd25a209770da08d3613de6e604c
JOE <joewttx@163.com>
2018-07-06 21:26:31 +0800

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title','weibo')</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title','weibo') Laravel 练习</title>
 </head>
 <body>
-@yield('content')
+
+@include('layouts._header');
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content');
+        @include('layouts._footer');
+    </div>
+</div>
+
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.default')
 @section('title','home')
 @section('content')
-    <h1>home</h1>
+    <div class="jumbotron">
+        <h1>Hello larabel</h1>
+        <p class="lead">
+            您现在看到的是weibo的示例首页 <a href="https://joewt.com">my blog</a>
+        </p>
+        <p>一切将从这里开始</p>
+        <p><a class="btn btn-lg btn-success" role="button" href="{{ route('signup') }}">现在注册</a></p>
+    </div>
 @stop
