042e29485202279ed80abc1eca902ab0a7e13b6b
JOE <joewttx@163.com>
2018-07-13 20:30:42 +0800

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,16 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
-    <title>@yield('title','weibo') Laravel 练习</title>
+    <title>@yield('title','weibo') Laravel Study</title>
 </head>
 <body>
 
-@include('layouts._header');
+@include('layouts._header')
 
 <div class="container">
     <div class="col-md-offset-1 col-md-10">
-        @yield('content');
-        @include('layouts._footer');
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
     </div>
 </div>
 
