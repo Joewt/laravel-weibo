235d6e3879203534e1bedb2a92f109f6dd41d609
JOE <joewttx@163.com>
2018-07-22 10:24:24 +0800

    完成登录和注册

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+
+class SessionsController extends Controller
+{
+    /**
+     * 显示登录页
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     *  用户登录
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' =>'required',
+        ]);
+
+        //使用email字段在数据库中查找，并对比password的结果
+        //记住我功能 attempt第二个参数，
+        if(Auth::attempt($credentials,$request->has('remember'))){
+            session()->flash('success',"登录成功");
+            return redirect()->route('users.show',[Auth::user()]);
+        } else {
+            session()->flash('danger',"您的邮箱或用户名不匹配");
+            return redirect()->back();
+        }
+        return;
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success',"您已成功退出");
+        return redirect('login');
+    }
+}
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,22 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
 
-
+    /**
+     * 注册页
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /**
+     * 显示用户信息
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show',compact('user'));
     }
 
-
+    /**
+     * 注册用户
+     * 用户注册成功后自动进行登录
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
         $this->validate($request,[
@@ -29,10 +43,13 @@ class UsersController extends Controller
         ]);
 
         $user = User::create([
-            'name'   =>  $request->name,
-            'email'  =>  $request->email,
-            'passowrd' => bcrypt($request->password),
+            'name'     =>  $request->name,
+            'email'    =>  $request->email,
+            'password' => bcrypt($request->password),
         ]);
+
+        Auth::login($user);
+
         session()->flash('success','注册成功');
         return redirect()->route('users.show',[$user]);
     }
